a02fc5d05c19ba68ede7280eefbe6808a2a01774
ezemog1996 <enrique_g_gomez@hotmail.com>
2022-01-31 13:27:31 -0500

    make all services discoverable

--- business_service/src/main/java/com/gomez/business_service/BusinessServiceApplication.java
+++ business_service/src/main/java/com/gomez/business_service/BusinessServiceApplication.java
@@ -1,10 +1,22 @@
 package com.gomez.business_service;
 
+import com.netflix.discovery.EurekaClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class BusinessServiceApplication {
+
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
+
+	@Value("${spring.application.name}")
+	private String appName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessServiceApplication.class, args);
