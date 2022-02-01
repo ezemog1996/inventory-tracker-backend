051f62848865937ff9181b226e1c4477800e3e15
ezemog1996 <enrique_g_gomez@hotmail.com>
2022-01-31 19:50:13 -0500

    missed these changes with the last commit

--- business_service/src/main/java/com/gomez/business_service/BusinessServiceApplication.java
+++ business_service/src/main/java/com/gomez/business_service/BusinessServiceApplication.java
@@ -1,22 +1,12 @@
 package com.gomez.business_service;
 
-import com.netflix.discovery.EurekaClient;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BusinessServiceApplication {
-
-	@Autowired
-	@Lazy
-	private EurekaClient eurekaClient;
-
-	@Value("${spring.application.name}")
-	private String appName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessServiceApplication.class, args);
--- location_service/src/main/java/com/gomez/location_service/LocationServiceApplication.java
+++ location_service/src/main/java/com/gomez/location_service/LocationServiceApplication.java
@@ -2,10 +2,12 @@ package com.gomez.location_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LocationServiceApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LocationServiceApplication.class, args);
 	}
