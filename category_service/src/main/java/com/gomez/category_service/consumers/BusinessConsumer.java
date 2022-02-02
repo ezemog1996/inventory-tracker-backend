a51e065a8566b35864dad33c520efc0590ba75b8
ezemog1996 <enrique_g_gomez@hotmail.com>
2022-02-02 14:24:38 -0500

    successfully used feign client to retrieve data from business service through the category service

---
+++ category_service/src/main/java/com/gomez/category_service/consumers/BusinessConsumer.java
@@ -0,0 +1,15 @@
+package com.gomez.category_service.consumers;
+
+import java.util.List;
+
+import com.gomez.category_service.dtos.Business;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "BUSINESS-SERVICE")
+public interface BusinessConsumer {
+
+    @GetMapping("/businesses")
+    public List<Business> getBusinesses();
+}
--- category_service/src/main/java/com/gomez/category_service/controllers/CategoryController.java
+++ category_service/src/main/java/com/gomez/category_service/controllers/CategoryController.java
@@ -1,5 +1,9 @@
 package com.gomez.category_service.controllers;
 
+import java.util.List;
+
+import com.gomez.category_service.consumers.BusinessConsumer;
+import com.gomez.category_service.dtos.Business;
 import com.gomez.category_service.models.Category;
 import com.gomez.category_service.services.CategoryService;
 
@@ -7,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +23,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     
     private CategoryService cs;
+    private BusinessConsumer bc;
 
     @Autowired
-    public CategoryController(CategoryService cs) {
+    public CategoryController(CategoryService cs, BusinessConsumer bc) {
         super();
         this.cs = cs;
+        this.bc = bc;
     }
 
     @PostMapping
     public ResponseEntity<Category> addCategory(@RequestBody Category category) {
         return new ResponseEntity<>(cs.addCategory(category), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/businesses")
+    public ResponseEntity<List<Business>> getBusinesses() {
+        return new ResponseEntity<>(bc.getBusinesses(), HttpStatus.OK);
     }
 }
